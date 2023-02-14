0a6d11076255ac09964965905a9c0ebfea9f651b
luyunji <luyunji@jd.com>
2023-02-14 16:12:00 +0800

    增加学习内容

---
+++ JavaBasic/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author by luyunji
+ * @since 2022-08-05 15:09
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
